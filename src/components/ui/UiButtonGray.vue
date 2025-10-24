1f81ccce05ab42b806caa2273b8ee6d00379acd8
mrtynnvv <dm346@mail.ru>
2025-10-24 16:53:54 +0300

    added ui-button-gray

---
+++ src/components/ui/UiButtonGray.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="ui-button">
+    {{ text }}
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps({ text: { type: String } })
+</script>
+
+<style scoped lang="scss">
+.ui-button {
+    background-color: rgba(0, 0, 0, 0.04);
+  border: none;
+  border-radius: 8px;
+  color: #2688eb;
+  font-size: 15px;
+  font-weight: 600;
+  height: 40px;
+  margin-top: 20px;
+  transition: 0.3s;
+  width: 100%;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+    cursor: pointer;
+    transition: 0.3s;
+  }
+}
+</style>
